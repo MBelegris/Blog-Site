d8c74b81a6e2f60b441524d2ffb7d4598e3105cf
Michael Belegris <b9015988@newcastle.ac.uk>
2021-10-31 14:07:39 +0000

    Created two directories: client and server. Client will deal with the front-end. Server will deal with the back-end.
    Began making server.js which currently outputs "Hello World!" when you go to localhost:3000

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send("Hello World!")
+});
+
+app.listen(port, () => console.log(`App listening on port: ${port} `))
