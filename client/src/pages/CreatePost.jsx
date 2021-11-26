c7c2a16e16bbfe52254a4e8eb3728ac2e1411340
Michael Belegris <b9015988@newcastle.ac.uk>
2021-11-26 11:20:35 +0000

    Implemented the Create Post page, user is now able to create posts through the front end

---
+++ client/src/pages/CreatePost.jsx
@@ -0,0 +1,67 @@
+import React, {Component} from "react";
+import apis from "../api/api";
+
+class createPost extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: "",
+            content: "",
+            author: "User",
+        }
+    }
+
+    handleChangeTitle = async event => {
+        const title = event.target.value;
+        this.setState({title});
+    }
+
+    handleChangeContent = async event => {
+        const content = event.target.value;
+        this.setState({content});
+    }
+
+    handleCreatePost = async () => {
+        const {title, content, author} = this.state;
+        //example: time,date-> 25/11/2021 at 11:23
+        const date = new Date();
+        const datePosted = date.getDate() + "/" + (date.getMonth()+1) + "/" + date.getFullYear() + " at " +
+            date.getHours() + ":" + date.getMinutes();
+
+        const payload = {title, content, author, datePosted};
+
+        console.log(`date posted ${payload}`);
+
+        await apis.insertPost(payload).then(res => {
+            window.alert("Successfully created a Post");
+            this.setState( {
+                title: "",
+                content:""
+            });
+        }).catch(error => {
+            console.log(error);
+        })
+
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Create A Post</h1>
+                <br/>
+                <br/>
+                <label>Title: </label>
+                <input type="text" placeholder="Title" onChange={this.handleChangeTitle}/>
+                <br/>
+                <label >Content: </label>
+                <br/>
+                <textarea name="content" id="content" cols="30" rows="10"
+                          placeholder="Content" onChange={this.handleChangeContent}/>
+                <br/>
+                <button onClick={this.handleCreatePost} type="submit">Post</button>
+            </div>
+        );
+    }
+}
+
+export default createPost;
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,5 +1,6 @@
 import Home from "./Home";
 import About from "./About";
 import Register from "./Register";
+import CreatePost from "./CreatePost";
 
-export { Home, About, Register};
+export { Home, About, Register, CreatePost};
