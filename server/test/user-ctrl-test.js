0d8b6036e68cfeaaec82b41d08492ebc35d44c7a
Michael Belegris <b9015988@newcastle.ac.uk>
2021-11-26 15:33:59 +0000

    Implemented testing for the User controller to check whether all CRUD works correctly

---
+++ server/test/user-ctrl-test.js
@@ -0,0 +1,131 @@
+//const assert = require("assert");
+//const mongoose = require('mongoose');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+chai.use(chaiHttp);
+const expect = chai.expect;
+
+let test_id;
+
+const User = require('../models/user-model');
+//const userCtrl = require('../controllers/user-ctrl');
+
+describe('Testing the user CRUD', function () {
+
+    it('should be invalid if name, username and pwd are null', function (done) {
+        let user = new User({
+            name: null,
+            username: null,
+            password: null
+        });
+
+        user.validate(function (err) {
+            expect(err.errors.name).to.exist;
+            expect(err.errors.username).to.exist;
+            expect(err.errors.password).to.exist;
+            done();
+        });
+    });
+
+    it('should be valid if name, username and pwd are not null', function (done) {
+        var user = new User({
+            name: "bob",
+            username: "boober",
+            password: "pwd"
+        });
+
+        user.validate(function () {
+            expect(user.name).to.exist;
+            expect(user.username).to.exist;
+            expect(user.password).to.exist;
+            done();
+        });
+    });
+
+    //Test user creation
+    it('should create a user in the db', function (done){
+
+        var user = new User({
+            name: "Test-user",
+            username: "test-username",
+            password: "Test-pwd"
+        });
+
+        chai.request('http://localhost:5000/api/user')
+            .post('/')
+            .set('content-type', 'application/json')
+            .send(user)
+            .then(function (res, err) {
+                test_id = res.body.id;
+                expect(res.status).to.equal(201);
+                done();
+            }).catch(function (err) {
+            throw err;
+        });
+    });
+
+    //test the get all users function
+    it('should get back all the users from the db', function (done){
+        chai.request('http://localhost:5000/api/users')
+            .get('/')
+            .then(function (res,err) {
+                expect(res).to.have.status(200);
+                done();
+            })
+            .catch(function (err) {
+                throw err;
+            })
+    });
+
+    //test get user by id
+    it('should get user by id', function (done) {
+        chai.request(`http://localhost:5000/api/user/${test_id}`)
+            .get('/')
+            .then(function (res, err) {
+                expect(res).to.have.status(200);
+                done();
+            }).catch(function (err) {
+                throw err;
+            })
+    });
+
+    //test update user
+    it('should update user', function (done) {
+        var user = new User({
+            name: "New-test User",
+            username: "test-username",
+            password: "Test-pwd"
+        });
+
+
+        chai.request(`http://localhost:5000/api/user/${test_id}`)
+            .put('/')
+            .set('content-type', 'application/json')
+            .send(user)
+            .then(function (res, err) {
+                expect(res).to.have.status(200);
+                done();
+            }).catch(function (err) {
+            throw err;
+        })
+    });
+
+    //test delete user by id
+    it('should delete user by id', function (done) {
+        chai.request(`http://localhost:5000/api/user/${test_id}`)
+            .delete('/')
+            .then(function (res, err) {
+                expect(res).to.have.status(200);
+                done();
+            }).catch(function (err) {
+            throw err;
+        })
+    });
+});
+
+
+/** To test
+ * 1. Update User
+ * 2. Delete User
+ */
