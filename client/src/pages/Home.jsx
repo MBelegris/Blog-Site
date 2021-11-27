6b10356d84cb5fe25a5802b49ae2f93d35a3bbf4
Michael Belegris <b9015988@newcastle.ac.uk>
2021-11-27 14:43:09 +0000

    Added a link to Create Post in the Home Page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,19 +1,24 @@
 import React, {Component} from "react";
+import {Link} from "react-router-dom";
 
 //import styled from "styled-components";
 
 class home extends Component {
     render() {
         return(
-            <div style={{
-                display: 'flex',
-                justifyContent: 'Left',
-                alignItems: 'Right',
-                height: '100vh'
-            }}>
+            <div>
                 <h1>
                     Home Page!
                 </h1>
+                <div>
+                    <Link to="/CreatePost">
+                        <button type="button">
+                            Create Post
+                        </button>
+                    </Link>
+                </div>
+                <br/>
+
             </div>
         );
     }
